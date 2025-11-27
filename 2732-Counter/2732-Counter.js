252bc423097dcde43b87ace695c8c5a3db8f7e7d
Geniuz <73511795+AhmadWajid@users.noreply.github.com>
2025-11-26 20:55:23 -0800

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 11/26/2025, 8:55:23 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
