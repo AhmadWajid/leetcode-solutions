0dc4b57ee50a852f5d77317bb85903e08252aa7e
Geniuz <73511795+AhmadWajid@users.noreply.github.com>
2025-11-26 21:34:40 -0800

    File updated at 11/26/2025, 9:34:40 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,18 +1,21 @@
-# Last updated: 11/26/2025, 8:55:28 PM
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums)+1)]
-        for num in nums:
-            if num in count:
-                count[num] += 1
-            else:
-                count[num] = 1
-        for num, num_count in count.items():
-            freq[num_count].append(num)
-        res = []
-        for i in range(len(freq)-1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if k == len(res):
-                    return res
+# Last updated: 11/26/2025, 9:34:40 PM
+1class Solution:
+2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+3        counter = {}
+4        freq_array = [[] for i in range(len(nums)+1)]
+5        result = []
+6        for num in nums:
+7            if num in counter:
+8                counter[num] += 1
+9            else:
+10                counter[num] = 1
+11        for key, v in counter.items():
+12            freq_array[v].append(key)
+13        for i in range(len(nums), 0, -1):
+14            for num in freq_array[i]:
+15                result.append(num)
+16                k -= 1
+17                if k == 0:
+18                    return result
+19                
+20
